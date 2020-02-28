9e2d1b6744b389132f449a2f65d39cbe5bc6b88d
Natalia Titova <bnmlucky@gmail.com>
2020-02-28 13:35:57 -0500

    created new instances of class Pet

--- Pet.js
+++ Pet.js
@@ -5,3 +5,8 @@ class Pet {
         this.breed = breed;
     }
 }
+
+const ernie = new Pet('dog', 1, 'pug');
+const yoshka = new Pet('cat', 6, 'brown tabby');
+const vera = new Pet('dog', 8, 'border collie');
+console.log(ernie);
