f24035c1463f0223c76bf39d1a4ca320fa486b45
Natalia Titova <bnmlucky@gmail.com>
2020-02-28 15:26:32 -0500

    added a  method to the class and called it

--- Pet.js
+++ Pet.js
@@ -1,12 +1,23 @@
 class Pet {
-    constructor(animal, age, breed) {
+    constructor(animal, age, breed, sound) {
         this.animal = animal;
         this.age = age;
         this.breed = breed;
+        this.sound = sound;
     }
+
+    speak() {
+        console.log(this.sound);
+    }
+
 }
 
-const ernie = new Pet('dog', 1, 'pug');
-const yoshka = new Pet('cat', 6, 'brown tabby');
-const vera = new Pet('dog', 8, 'border collie');
+const ernie = new Pet('dog', 1, 'pug', 'yip yip');
+const yoshka = new Pet('cat', 6, 'brown tabby', 'meow meow');
+const vera = new Pet('dog', 8, 'border collie', 'woof woof');
+
 console.log(ernie);
+
+ernie.speak();
+yoshka.speak();
+vera.speak();
