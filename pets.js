795e1cdd9c508cae872517720243cb7a0d9990b0
Natalia Titova <bnmlucky@gmail.com>
2020-02-28 11:02:42 -0500

    added more objects in order to create a class out of them in the future

--- pets.js
+++ pets.js
@@ -13,3 +13,35 @@ ernie['bark']();
 
 var prop = 'breed';
 ernie[prop];
+
+ernie.age = 2;
+ernie['age'] = 2;
+ernie.color = 'black';
+console.log(ernie);
+
+const vera = {
+    animal: 'dog',
+    age: 8,
+    breed: 'Border Collie',
+    bark: function () {
+        console.log('Woof!');
+    }
+}
+
+const scofield = {
+    animal: 'dog',
+    age: 6,
+    breed: 'Doberman',
+    bark: function () {
+        console.log('Woof!');
+    }
+}
+
+const edel = {
+    animal: 'dog',
+    age: 7,
+    breed: 'German Shorhaired Pointer',
+    bark: function () {
+        console.log('Woof!');
+    }
+}
