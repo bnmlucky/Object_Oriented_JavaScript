77a0d0ee71af3ba6e63e159eef0694eaa55678d5
Natalia Titova <bnmlucky@gmail.com>
2020-03-04 15:32:32 -0500

    created Owner object

--- Pet.js
+++ Pet.js
@@ -33,6 +33,22 @@ class Pet {
 
 }
 
+class Owner {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    set phone(phone) {
+        const phoneNormalized = phone.replace(/[^0-9]/g, '');
+        this._phone = phoneNormalized;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+}
+
 const ernie = new Pet('dog', 1, 'pug', 'yip yip');
 const yoshka = new Pet('cat', 6, 'brown tabby', 'meow meow');
 const vera = new Pet('dog', 8, 'border collie', 'woof woof');
@@ -45,5 +61,7 @@ vera.speak();
 
 console.log(ernie);
 
-ernie.owner = 'Ashley';
-console.log(ernie.owner);
+ernie.owner = new Owner('Ashley', '123 Main Street');
+ernie.owner.phone = '(646) 593-2771';
+console.log(ernie.owner.name);
+console.log(ernie.owner.phone);
